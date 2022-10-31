b1a82a98d6095f7e82a0ba760527c2e50ee79e63
Jim <Jimjnguy@gmail.com>
2022-10-30 20:31:56 -0400

    updated random number logic

--- my-app/src/store/people.js
+++ my-app/src/store/people.js
@@ -3,23 +3,21 @@ const SET_PERSON = "SET_PERSON";
 //==========actions============
 // set Person in store
 
-export const setPerson = (listPerson) => {
+export const setPerson = (person) => {
   return {
     type: SET_PERSON,
-    payload: listPerson,
+    payload: person,
   };
 };
 
 //========= thunk action creators=========
 
 export const getPerson = () => async (dispatch) => {
-  const personNumber = Math.random(50);
+  const personNumber = Math.floor(Math.random() * (50 - 1 + 1) + 1);
   const response = await fetch(`https://swapi.dev/api/people/${personNumber}`);
   if (response.ok) {
     const { message } = await response.json();
-    const person = Object.keys(message);
-
-    dispatch(setPerson(person));
+    dispatch(setPerson(message));
   } else return response.json();
 };
 
